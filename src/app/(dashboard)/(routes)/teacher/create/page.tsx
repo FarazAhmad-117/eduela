d5cada90f8467de5462d015214c2bd0e594620bb
Faraz Ahmad <farazahmad31048@gmail.com>
2024-08-10 19:53:59 +0500

    created layout

---
+++ src/app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+import * as z from 'zod';
+
+
+
+const CreateCoursePage = () => {
+    return (
+        <div>CreateCoursePage</div>
+    )
+}
+
+export default CreateCoursePage
