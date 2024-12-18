5195ba63dc4f9f1213d7d791623d0922e8c4053f
FarazAhmad-117 <farazahmad31048@gmail.com>
2024-12-19 00:33:27 +0500

    some code

--- src/app/(course)/courses/[courseId]/_components/course-navbar.tsx
+++ src/app/(course)/courses/[courseId]/_components/course-navbar.tsx
@@ -1,6 +1,7 @@
 import NavbarRoutes from "@/components/navbar-routes";
 import { Chapter, Course, UserProgress } from "@prisma/client";
 import React from "react";
+import CourseMobileSidebar from "./course-mobile-sidebar";
 
 interface CourseNavbarProps {
   course: Course & {
@@ -14,6 +15,7 @@ interface CourseNavbarProps {
 const CourseNavbar = ({ course, progressCount }: CourseNavbarProps) => {
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm ">
+      <CourseMobileSidebar course={course} progressCount={progressCount} />
       <NavbarRoutes />
     </div>
   );
---
+++ src/app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChapterPage = () => {
+  return <div></div>;
+};
+
+export default ChapterPage;
